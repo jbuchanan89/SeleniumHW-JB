9d592da79f27a88f253c8d2228d553f2a00f5f7c
jbuchanan89 <jen.rumingan@gmail.com>
2024-09-21 20:32:02 -0400

    Homework update

--- src/main/java/org/example/WebDriverAssignment.java
+++ src/main/java/org/example/WebDriverAssignment.java
@@ -1,7 +1,25 @@
 package org.example;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverAssignment {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
 
+        String title = driver.getTitle();
+        System.out.println(title);
+
+        driver.get("https://www.syntaxprojects.com");
+        String title2 = driver.getTitle();
+        System.out.println(title2);
+
+         driver.navigate().back();
+         Thread.sleep(2000);
+         driver.navigate().refresh();
+
+         driver.quit();
+    }
 }
