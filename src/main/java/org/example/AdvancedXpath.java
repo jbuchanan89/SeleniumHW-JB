8d4f69dad80728b9f86208a0eb7e42d3ba5ca78d
jbuchanan89 <jen.rumingan@gmail.com>
2024-09-23 21:59:44 -0400

    HW update

--- src/main/java/org/example/AdvancedXpath.java
+++ src/main/java/org/example/AdvancedXpath.java
@@ -1,2 +1,32 @@
-package org.example;public class AdvancedXpath {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AdvancedXpath {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.syntaxprojects.com/advanceXpath-homework.php");
+
+        WebElement book1 = driver.findElement(By.xpath("//input[@id='least-favorite']/preceding-sibling::input[2]"));
+        book1.sendKeys("The Women");
+
+        WebElement book2 = driver.findElement(By.xpath("//input[@id='least-favorite']/preceding-sibling::input[1]"));
+        book2.sendKeys("Harry Potter");
+
+        WebElement book3 = driver.findElement(By.xpath("//input[@id='least-favorite']"));
+        book3.sendKeys("The God of the Woods");
+
+        WebElement favBook = driver.findElement(By.xpath("//input[@id='favouriteBook']"));
+        favBook.sendKeys("The God of the Woods");
+
+        WebElement favBook2 = driver.findElement(By.xpath("//input[@id='favouriteBook']/following-sibling::input[1]"));
+        favBook2.sendKeys("Harry Potter");
+
+        WebElement favBook3 = driver.findElement(By.xpath("//input[@id='favouriteBook']/following-sibling::input[2]"));
+        favBook3.sendKeys("The Women");
+    }
 }
--- src/main/java/org/example/RelativeXpathAssignment.java
+++ src/main/java/org/example/RelativeXpathAssignment.java
@@ -1,2 +1,51 @@
-package org.example;public class RelativeXpathAssignment {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RelativeXpathAssignment {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.syntaxprojects.com/advanceXpath-homework.php");
+
+        WebElement hobbies = driver.findElement(By.xpath("//input[@id='yourHobbiesId']"));
+        hobbies.sendKeys("Hiking");
+
+        WebElement clickHere = driver.findElement(By.xpath("//button[@id='display_text']"));
+        clickHere.click();
+
+        WebElement favoriteMovies = driver.findElement(By.xpath("//input[@id='favoriteMoviesId']"));
+        favoriteMovies.sendKeys("Free Willy");
+
+        WebElement paragraph = driver.findElement(By.xpath("//label[contains(text(), 'amet consectetur adipisicing elit')]"));
+        String fullText = paragraph.getText();
+        System.out.println(fullText);
+
+        WebElement city = driver.findElement(By.xpath("//input[@id='yourCity']"));
+        city.sendKeys("Falls Church");
+
+        WebElement personalEmail = driver.findElement(By.xpath("//input[@id='private_email_id']"));
+        personalEmail.sendKeys("Jbuchanan@gmail.com");
+
+        WebElement officeEmail = driver.findElement(By.xpath("//input[@id='office_email_id']"));
+        officeEmail.sendKeys("Jbuchanan@work.com");
+
+        WebElement professionalEmail = driver.findElement(By.xpath("//input[@id='professional_email_id']"));
+        professionalEmail.sendKeys("Jbuchanan@prof.com");
+
+        WebElement clientName = driver.findElement(By.xpath("//input[@id='clientName' and  @type='text']"));
+        clientName.sendKeys("Client Name");
+
+        WebElement clientID = driver.findElement(By.xpath("//input[@id='clientId' and  @type='text']"));
+        clientID.sendKeys("1213123123");
+
+        WebElement streetNo = driver.findElement(By.xpath("//input[@name='StreetNo' and  @type='text']"));
+        streetNo.sendKeys("445");
+
+        WebElement houseNo = driver.findElement(By.xpath("//input[@name='HouseNo' and  @type='text']"));
+        houseNo.sendKeys("78903");
+    }
 }
