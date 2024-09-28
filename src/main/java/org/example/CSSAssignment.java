490ba45335475ded9c8b344fbaf85a9bb334d7fe
jbuchanan89 <jen.rumingan@gmail.com>
2024-09-28 10:33:36 -0400

    HW update

--- src/main/java/org/example/CSSAssignment.java
+++ src/main/java/org/example/CSSAssignment.java
@@ -1,2 +1,38 @@
-package org.example;public class CSSAssignment {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CSSAssignment {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://syntaxprojects.com/cssSelector-homework.php");
+
+        WebElement userID = driver.findElement(By.cssSelector("input#UserID"));
+        userID.sendKeys("Jedrfasdfa");
+
+        WebElement userName = driver.findElement(By.cssSelector("input#UserName"));
+        userName.sendKeys("Jbuchanan");
+
+        WebElement lectureIntro = driver.findElement(By.cssSelector("input[name^='LectureIntro']"));
+        lectureIntro.sendKeys("Intro to Lecture");
+
+        WebElement rayFeedback = driver.findElement(By.cssSelector("input#FeedbackFromRay"));
+        rayFeedback.sendKeys("It was great!");
+
+        WebElement duckyFeedback = driver.findElement(By.cssSelector("input#FeedbackfromDucky"));
+        duckyFeedback.sendKeys("It was terrible");
+
+        WebElement clientID = driver.findElement(By.cssSelector("input[name^='ClientID']"));
+        clientID.sendKeys("423423423432423");
+
+        WebElement email = driver.findElement(By.cssSelector("input[name^='email']"));
+        email.sendKeys("hello@gmail.com");
+
+        WebElement courseTopic = driver.findElement(By.cssSelector("input[name^='CourseTopic']"));
+        courseTopic.sendKeys("Testing");
+    }
 }
